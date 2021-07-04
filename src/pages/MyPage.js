8df29b95f46bc2ea2344f255fe749103efdd3521
IJMilk-WS <rladntjd320@gmail.com>
2021-07-04 20:06:59 +0900

    [feat] / #37 / mypage html markup

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -3,7 +3,25 @@ import React from "react";
 const MyPage = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <label>이메일</label>
+        <div></div>
+        <button></button>
+      </div>
+      <div>
+        <label>유저이름</label>
+        <div></div>
+        <button></button>
+      </div>
+      <div>
+        <label>비밀번호</label>
+        <button></button>
+      </div>
+      <div>
+        <label>관심 카테고리</label>
+        <button></button>
+        <ul></ul>
+      </div>
     </div>
   );
 };
