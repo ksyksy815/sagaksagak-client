3dda254bc147610ac76f78a8b00fee86f7f74e7c
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-07-16 13:23:22 +0900

    [Fix]/Deleted unused variables

--- src/components/ChatroomTodo.js
+++ src/components/ChatroomTodo.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { setAccessToken, logOut, createTodo, checkTodo, deleteTodo } from '../actions/index'
