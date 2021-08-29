b17bd730c9946dd801fc06851e383f50b95939cf
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-29 21:39:31 +0900

    [Update] Applied the new inner aside for study log page

--- src/pages/Studylog.js
+++ src/pages/Studylog.js
@@ -6,19 +6,35 @@ import TodoList from '../components/Todos/TodoList'
 import StudyRecords from '../components/StudyRecords/StudyRecords'
 
 const StudylogWrapper = styled.div`
-  box-sizing: border-box;
-  width: 100vw;
-  height: auto;
-  min-height: 100vh;
-  padding-top: 87px; /*나브바 높이 뺀 것 */
-  padding-bottom: 2rem;
+  width: 100%;
+  max-width: 1440px;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  padding: 2rem;
+  padding-top: 5rem;
 
-  @media (max-width: 650px) {
-    padding-bottom: 0;
+  @media (max-width: 900px) {
+    padding: 5rem 0.5rem 0.5rem 0.5rem;
+    flex-direction: column;
+  }
+`
+
+const Board = styled.main`
+  flex: 1 1 auto;
+  max-width: 1440px;
+  align-self: stretch;
+  background: rgba(255,255,255, 0.3);
+  backdrop-filter: blur(10px);
+  display: flex;
+  box-shadow: 2px 2px 20px 5px rgba(0, 0, 0, 0.1);
+  
+  @media (max-width: 900px) {
+    flex-direction: column;
+    justify-content: flex-start;
+    overflow-y: scroll;
+    
   }
 `
 
@@ -26,14 +42,16 @@ export default function Studylog() {
   return (
     <StudylogWrapper>
       <StudylogNav />
-      <Switch>
-        <Route exact path='/studylog'>
-          <TodoList />
-        </Route>
-        <Route path='/studylog/records'>
-          <StudyRecords />
-        </Route>
-      </Switch>
+      <Board>
+        <Switch>
+          <Route exact path='/studylog'>
+            <TodoList />
+          </Route>
+          <Route path='/studylog/records'>
+            <StudyRecords />
+          </Route>
+        </Switch>
+      </Board>
     </StudylogWrapper>
   )
 }
